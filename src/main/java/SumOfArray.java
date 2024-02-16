066e2f271b07c9229208e8dde639ccb0148306e1
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 19:09:52 +0200

    Initial commit

---
+++ src/main/java/SumOfArray.java
@@ -0,0 +1,19 @@
+
+public class SumOfArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(array));
+    }
+
+    public static int sumOfNumbersInArray(int[] array) {
+        int sum = 0;
+        int iterate = 0;
+        while (iterate < array.length) {
+            sum = sum + array[iterate];
+            iterate++;
+        }
+        return sum;
+    }
+}
